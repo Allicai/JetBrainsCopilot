7058c65b0d4fd26a6aab519cf3f191ce0c6fbfb3
GitHub Action <action@github.com>
2025-06-05 23:47:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/allicai/jetbrainscopilot/services/MyProjectService.kt
+++ src/main/kotlin/com/github/allicai/jetbrainscopilot/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.allicai.jetbrainscopilot.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.allicai.jetbrainscopilot.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
